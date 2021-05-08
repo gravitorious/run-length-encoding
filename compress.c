b85ea23b7479c161c96ec74155186d9f7758c639
Mavrogeneiadis Nikolaos <nikolasmay1@yahoo.gr>
2021-05-08 15:20:08 +0300

    Solve openFile

--- compress.c
+++ compress.c
@@ -13,26 +13,34 @@ typedef struct{
 }run;
 
 
-int countChars(){
-
+/*
+ * counting runs of the uncompressed file
+ */
+int countRuns(FILE *fp){
+    char ch;
+    while(!feof(fp)){
+        ch = fgetc(fp);
+        if(feof(fp)) break;
+        printf("%c\n", ch);
+    }
+    return 0;
 }
 
-int openFile(FILE *fp){
-    fp = fopen(UNCOMPRESSED_FILE, "r");
-    if(fp == NULL) return FALSE;
+int openFile(FILE **fp){
+    *fp = fopen(UNCOMPRESSED_FILE, "r");
+    if(*fp == NULL) return FALSE;
     else return TRUE;
 }
 
-
-
 int main(int argc, char *argv[]){
-    FILE *fp;
-    int is_opened = openFile(fp);
+    FILE *fp = NULL;
+    int is_opened = openFile(&fp);
     if(is_opened == FALSE){
         puts("File can't be opened for some reason");
         exit(1);
     }
-    printf("File %s opened successfully", UNCOMPRESSED_FILE);
+    printf("File %s opened successfully...", UNCOMPRESSED_FILE);
+    int result = countRuns(fp);
 
     return 0;
 }
